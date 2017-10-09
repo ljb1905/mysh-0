5119d46fedf2f00d7bbbcc5bed10afc1f369b0a2
LeeJongbin <ljb1905@ajou.ac.kr>
2017-10-09 15:26:41 +0900

    utils.c

--- src/utils.c
+++ src/utils.c
@@ -3,5 +3,14 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+  int idx=0;//
+
+  strtok((char*)command, "\n");
+  strtok((char*)command, " ");
+  
+  (*argv) = (char**)malloc(sizeof(char*)*20);
+  for(int i=0;i<20;i++)
+  {
+    (*argv)[i] = (char*)calloc(sizeof(char),100);
+  }
 }
