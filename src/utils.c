1b64f08be532991b261d6d592d9705b398499621
LeeJongbin <ljb1905@ajou.ac.kr>
2017-10-09 16:01:24 +0900

    temp utils.c complete

--- src/utils.c
+++ src/utils.c
@@ -3,14 +3,28 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  int idx=0;//
+  int idx=0;
+  char*tmp;
+  char*ptr[20]={};
 
-  strtok((char*)command, "\n");
-  strtok((char*)command, " ");
-  
   (*argv) = (char**)malloc(sizeof(char*)*20);
-  for(int i=0;i<20;i++)
+  for(idx=0;idx<20;i++)
   {
-    (*argv)[i] = (char*)calloc(sizeof(char),100);
+    (*argv)[idx] = (char*)calloc(sizeof(char),100);
+  }
+
+  tmp = strtok((char*)command, "\n");
+  tmp = strtok((char*)command, " ");
+  
+  for(idx=0;tmp!=NULL;idx++)
+  {
+    ptr[idx] = tmp;
+    argv[idx] = &ptr[idx];
+    strcpy((*argv)[i],tmp);
+    tmp = strtok(NULL, "\n");
+    tmp = strtok(NULL, " ");
+  }
+  *argc = idx;
+  argc = &idx;
   }
 }
