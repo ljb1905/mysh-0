4c1c02dcd2564846b594f20eeef358c2891a7b85
LeeJongbin <ljb1905@ajou.ac.kr>
2017-10-09 16:55:56 +0900

    tmp commands.c complete

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,30 @@
 #include <string.h>
-
+#include <stdio.h>
+#include <unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-  return 0;
+  int tmp = chdir(argv[1]);
+  return tmp;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+  char tmp[100] = {};
+  getcwd(tmp,100);
+  printf("%ss\n",tmp);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"cd"))==0 && argc == 2) return 1;
+  return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(strcmp(argv[0],"pwd"))==0 && argc == 1) return 1;
+  return 0;
 }
